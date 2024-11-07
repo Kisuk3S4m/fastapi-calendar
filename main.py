b5978ae08fac60e42cf942ead1abde63f95b56b7
Kisuk3S4m <homelessorpenguin@gmail.com>
2024-11-07 22:55:00 +0100

    Load environment variables and update Uvicorn run configuration

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from `.env` file
+load_dotenv()
+
+# Create FastAPI app
 app = FastAPI()
 
+# Define a route for the root endpoint
 @app.get("/")
 def read_root():
     return {
         "message": "Welcome to the application."
     }
 
+# Run the app with Uvicorn
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080)
+    host = os.getenv("APP_HOST", "127.0.0.1")
+    reload = os.getenv("APP_RELOAD", "1") == "1"
+    uvicorn.run("main:app", host=host, port=8080, reload=reload)
